5ed97b147bd1da442676b44c979c60715b0628b5
Laszlo Fogas <laszlo@gimlet.io>
2023-01-02 20:51:41 +0100

    Fixe

--- src/components/Raw.jsx
+++ src/components/Raw.jsx
@@ -2,7 +2,7 @@
 export function Raw({link}) {
     return (
       <>
-        <blockquote class="twitter-tweet">
+        <blockquote className="twitter-tweet">
           <a href={link} />
         </blockquote>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script> 
