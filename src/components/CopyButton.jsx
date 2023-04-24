85c75420f3e74d0e129819746938602f995f8bed
dzsak <sisakadam@gmail.com>
2023-04-24 16:23:55 +0200

    Copy button style

--- src/components/CopyButton.jsx
+++ src/components/CopyButton.jsx
@@ -22,7 +22,7 @@ const CopyButton = ({ text, style }) => {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
             </svg>
             {isCopied && (
-                <div className="absolute top-0 right-full mr-2 mt-2">
+                <div className="absolute bottom-6 left-0">
                     <div className="p-2 bg-indigo-600 text-white inline-block rounded">
                         Copied!
                     </div>
