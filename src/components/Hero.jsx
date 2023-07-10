dc2872ed5eeb19e23a86f692ef717eacf9fa374b
Laszlo Fogas <laszlo@gimlet.io>
2023-07-10 21:33:26 +0200

    Ideas

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,13 +34,13 @@ export function Hero() {
         <div className="relative px-6 lg:px-8">
           <div className="mx-auto max-w-4xl pt-20 pb-32 sm:pt-48 sm:pb-40">
             <div>
-              <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+              {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-100/10">
                   <span className="text-gray-600 dark:text-gray-300">
                     Announcing our SaaS Early Access. <a href="/blog/announcing-the-gimlet-saas-early-access" onClick={() => Fathom.trackGoal('KDOSOUVD', 0)} className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Read more <span aria-hidden="true">&rarr;</span></a>
                   </span>
                 </div>
-              </div>
+              </div> */}
               <div>
                 <p className="mb-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">
                   Adopting Kubernetes is a big endevaur. But it is the deployment platform today.
@@ -50,9 +50,9 @@ export function Hero() {
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">
                   With Gimlet you can start with one-click-deploys, then stair-step your way to a gitops based application delivery platform when you need to.
-                  Gimlet features the best of open-source out of the box, it is ejectable and open-source. It is the tooling you would build - given enough time.
+                  It features the best of open-source out of the box.<br /> Gimlet is ejectable and open-source.
                 </p>
-                <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
+                {/* <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
                   <a
                     href="https://accounts.gimlet.io/signup/"
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
@@ -67,7 +67,7 @@ export function Hero() {
                   >
                     Read more
                   </a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
