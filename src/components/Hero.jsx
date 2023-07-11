6cae4ff6c938509cac42204f3b96254074b5da3e
Laszlo Fogas <laszlo@gimlet.io>
2023-07-11 07:58:58 +0200

    Adjustments

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ export function Hero() {
     <div className="overflow-hidden dark:-mb-32 dark:-mt-[4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:-mt-[4.75rem] dark:lg:pt-[4.75rem]">
       <main>
         <div className="relative px-6 lg:px-8">
-          <div className="mx-auto max-w-4xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+          <div className="mx-auto max-w-4xl">
             <div>
               {/* <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-100/10">
@@ -50,24 +50,36 @@ export function Hero() {
                 </h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">
                   With Gimlet you can start with one-click-deploys, then stair-step your way to a gitops based application delivery platform when you need to.
-                  It features the best of open-source out of the box.<br /> Gimlet is ejectable and open-source.
+                  It is ejectable and features the best of open-source out of the box.
                 </p>
-                {/* <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
-                  <a
-                    href="https://accounts.gimlet.io/signup/"
-                    className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                    onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
-                  >
-                    Start deploying now
-                  </a>
+                <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
                   <a
                     href="/docs"
-                    className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20"
+                    className="inline-block rounded-lg bg-indigo-600 px-8 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                     onClick={() => Fathom.trackGoal('VEWYCI7B', 0)}
                   >
-                    Read more
+                    Documentation
                   </a>
-                </div> */}
+                  {/* <a
+                    href="https://accounts.gimlet.io/signup/"
+                    className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20"
+                    onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
+                  >
+                    Sign up
+                  </a> */}
+                </div>
+                <div className="mt-8 p-4 bg-gray-900 text-yellow-100 rounded-lg">
+                  <div className="whitespace-pre-wrap">
+                    <p className="text-green-100"># Install Gimlet on any Kubernetes cluster.</p>
+                    <p className="text-green-100"># Even on a cluster running on your laptop - we recommend using <a className="underline cursor-pointer">k3d</a>. </p>
+                    <p className="text-green-100"># Install with:</p>
+
+                    <p className="font-semibold my-4">kubectl apply -f https://raw.githubusercontent.com/gimlet-io/gimlet/main/deploy/gimlet.yaml</p>
+
+                    <p className="text-green-100"># Or start deploying your applications on our <a href="" className="underline cursor-pointer">hosted version</a>.</p>
+                    <p className="text-green-100"># We have a free tier, that <a href="" className="underline cursor-pointer">won't make you think</a> every corner.</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,49 +9,19 @@ export function HomePage({ className, tabs, code, language }) {
 
   return (
     <>
-      <Link href="/">
-        <a className="block lg:w-auto">
-          <span className="sr-only">Home page</span>
-          <img src="/logo.svg" alt="Gimlet" className='h-8 sm:h-10 w-auto block dark:hidden' />
-          <img src="/logo-dark.svg" alt="Gimlet" className='h-8 sm:h-10 w-auto hidden dark:block' />
-        </a>
-      </Link>
-      <Hero />
-      <div className="mx-auto max-w-4xl">
-      <div className="p-4 bg-gray-900 text-yellow-100 rounded-lg">
-        <div className="whitespace-pre-wrap">
-          <p className="text-green-100"># Install Gimlet on any Kubernetes cluster.</p>
-          <p className="text-green-100"># Even on a cluster running on your laptop - we recommend using <a className="underline cursor-pointer">k3d</a>. </p>
-          <p className="text-green-100"># Install with:</p>
-
-          <p className="font-semibold my-4">helm template gimlet-io/gimlet | kubectl apply -f -</p>
-
-          <p className="text-green-100"># Or get started deploying your applications on our <a href="" className="underline cursor-pointer">hosted version</a>.</p>
-          <p className="text-green-100"># We have a free tier, that <a href="" className="underline cursor-pointer">won't make you think</a> every corner</p>
-        </div>
+      <div className="text-center pt-12 sm:pt-16">
+        <Link href="/">
+          <a>
+            <span className="sr-only">Home page</span>
+            <img src="/logo.svg" alt="Gimlet" className='h-10 sm:h-16 inline dark:hidden' />
+            <img src="/logo-dark.svg" alt="Gimlet" className='h-10 sm:h-16 hidden dark:inline' />
+          </a>
+        </Link>
       </div>
+      <div className="pt-20 pb-32 sm:pt-20 sm:pb-32">
+        <Hero />
       </div>
-      <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
-        <a
-          href="https://accounts.gimlet.io/signup/"
-          className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-          onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
-        >
-          Documentation
-        </a>
-        <a
-          href="/docs"
-          className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20"
-          onClick={() => Fathom.trackGoal('VEWYCI7B', 0)}
-        >
-          Documentation
-          USP: deploy any commit
-          USP: know what is who deployed what and when, see commits and not a real auditlog, something gitops hasn't delivered
-          USP: Ejectable, composable design. Gimlet is Flux underneath, plus workfllows that otherwise you would have to build
-          USP: gitops unbroken ci feedback loop
-          USP: stair-step, as little, no steep ramp up
-        </a>
-      </div>
+      
       {/* <Message />
       <Shot />
       <Gitops />
