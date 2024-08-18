d221d3e1b83e54efaddcf6bedbe02a2624ad10c8
arvind singharpuria <arvind.020606@gmail.com>
2024-08-18 22:19:51 +0530

    docs: added vue static deploymen docs

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,7 +6,7 @@ import { Layout } from '@/components/Layout'
 
 import 'focus-visible'
 import '@/styles/tailwind.css'
-import * as Fathom from "fathom-client";
+import * as Fathom from 'fathom-client'
 
 const navigation = [
   {
@@ -19,60 +19,136 @@ const navigation = [
   {
     title: 'Frontend Tutorials',
     links: [
-      { title: 'Next.js Deployment', href: '/docs/frontend/next-js-deployment-tutorial' },
-      { title: 'React Deployment', href: '/docs/frontend/react-deployment-tutorial' },
-      { title: 'Remix Deployment', href: '/docs/frontend/remix-deployment-tutorial' },
-      { title: 'Static Site Deployment', href: '/docs/frontend/static-site-deployment-tutorial' },
+      {
+        title: 'Next.js Deployment',
+        href: '/docs/frontend/next-js-deployment-tutorial',
+      },
+      {
+        title: 'React Deployment',
+        href: '/docs/frontend/react-deployment-tutorial',
+      },
+      {
+        title: 'Remix Deployment',
+        href: '/docs/frontend/remix-deployment-tutorial',
+      },
+      {
+        title: 'Static Site Deployment',
+        href: '/docs/frontend/static-site-deployment-tutorial',
+      },
+      {
+        title: 'Vue.js Deployment',
+        href: '/docs/frontend/vue-deployment-tutorial',
+      },
     ],
   },
   {
     title: 'Backend Tutorials',
     links: [
-      { title: 'Laravel Deployment', href: '/docs/backend/laravel-deployment-tutorial' },
-      { title: 'Django Deployment', href: '/docs/backend/django-deployment-tutorial' },
+      {
+        title: 'Laravel Deployment',
+        href: '/docs/backend/laravel-deployment-tutorial',
+      },
+      {
+        title: 'Django Deployment',
+        href: '/docs/backend/django-deployment-tutorial',
+      },
     ],
   },
   {
     title: 'AI Deployment Tutorials',
     links: [
-      { title: 'Flowise Deployment', href: '/docs/ai-deployments/flowise-deployment-tutorial' },
-      { title: 'Dockerfile Deployments', href: '/docs/ai-deployments/dockerfile-deployment-tutorial' },
-      { title: 'Streamlit Deployment', href: '/docs/ai-deployments/streamlit-deployment-tutorial' },
-      { title: 'Jupyter Notebook Deployments', href: '/docs/ai-deployments/jupyter-notebook-deployment-tutorial' },
-      { title: 'vLLM Deployment', href: '/docs/ai-deployments/vllm-deployment-tutorial' },
-      { title: 'Hugging Face Model Deployments', href: '/docs/ai-deployments/hugging-face-model-deployment-tutorial' },
+      {
+        title: 'Flowise Deployment',
+        href: '/docs/ai-deployments/flowise-deployment-tutorial',
+      },
+      {
+        title: 'Dockerfile Deployments',
+        href: '/docs/ai-deployments/dockerfile-deployment-tutorial',
+      },
+      {
+        title: 'Streamlit Deployment',
+        href: '/docs/ai-deployments/streamlit-deployment-tutorial',
+      },
+      {
+        title: 'Jupyter Notebook Deployments',
+        href: '/docs/ai-deployments/jupyter-notebook-deployment-tutorial',
+      },
+      {
+        title: 'vLLM Deployment',
+        href: '/docs/ai-deployments/vllm-deployment-tutorial',
+      },
+      {
+        title: 'Hugging Face Model Deployments',
+        href: '/docs/ai-deployments/hugging-face-model-deployment-tutorial',
+      },
     ],
   },
   {
     title: 'Deployments',
     links: [
-      { title: 'Preview Deployments', href: '/docs/deployments/preview-deployments' },
+      {
+        title: 'Preview Deployments',
+        href: '/docs/deployments/preview-deployments',
+      },
       { title: 'Rollbacks', href: '/docs/deployments/rollbacks' },
-      { title: 'Automated Deployments', href: '/docs/deployments/automated-deployments' },
+      {
+        title: 'Automated Deployments',
+        href: '/docs/deployments/automated-deployments',
+      },
     ],
   },
   {
     title: 'Deployment settings',
     links: [
-      { title: 'Introduction', href: '/docs/deployment-settings/deployment-configuration' },
-      { title: 'Container Build Settings', href: '/docs/deployment-settings/image-settings' },
-      { title: 'Container Registries', href: '/docs/deployment-settings/registries' },
-      { title: 'Social Authentication', href: '/docs/deployment-settings/social-authentication' },
+      {
+        title: 'Introduction',
+        href: '/docs/deployment-settings/deployment-configuration',
+      },
+      {
+        title: 'Container Build Settings',
+        href: '/docs/deployment-settings/image-settings',
+      },
+      {
+        title: 'Container Registries',
+        href: '/docs/deployment-settings/registries',
+      },
+      {
+        title: 'Social Authentication',
+        href: '/docs/deployment-settings/social-authentication',
+      },
       { title: 'DNS', href: '/docs/deployment-settings/dns' },
-      { title: 'Port Forwarding', href: '/docs/deployment-settings/port-forwarding' },
+      {
+        title: 'Port Forwarding',
+        href: '/docs/deployment-settings/port-forwarding',
+      },
       { title: 'HTTPS', href: '/docs/deployment-settings/https' },
       { title: 'Secrets', href: '/docs/deployment-settings/secrets' },
       { title: 'Volumes', href: '/docs/deployment-settings/volumes' },
-      { title: 'Resource Usage', href: '/docs/deployment-settings/resource-usage' },
-      { title: 'Chat Notifications', href: '/docs/deployment-settings/chat-notifications' },
-      { title: 'Creating a Custom Template', href: '/docs/deployment-settings/custom-template' },
+      {
+        title: 'Resource Usage',
+        href: '/docs/deployment-settings/resource-usage',
+      },
+      {
+        title: 'Chat Notifications',
+        href: '/docs/deployment-settings/chat-notifications',
+      },
+      {
+        title: 'Creating a Custom Template',
+        href: '/docs/deployment-settings/custom-template',
+      },
     ],
   },
   {
     title: 'Environment settings',
     links: [
-      { title: 'Introduction', href: '/docs/environment-settings/introduction' },
-      { title: 'Component updates', href: '/docs/environment-settings/component-updates' },
+      {
+        title: 'Introduction',
+        href: '/docs/environment-settings/introduction',
+      },
+      {
+        title: 'Component updates',
+        href: '/docs/environment-settings/component-updates',
+      },
     ],
   },
   {
@@ -81,7 +157,10 @@ const navigation = [
       { title: 'Loki', href: '/docs/monitoring/loki' },
       { title: 'Grafana', href: '/docs/monitoring/grafana' },
       { title: 'Prometheus', href: '/docs/monitoring/prometheus' },
-      { title: 'Integated Kubernetes Alerts', href: '/docs/monitoring/integrated-kubernetes-alerts' },
+      {
+        title: 'Integated Kubernetes Alerts',
+        href: '/docs/monitoring/integrated-kubernetes-alerts',
+      },
     ],
   },
   {
@@ -94,23 +173,41 @@ const navigation = [
   {
     title: 'Kubernetes Resources',
     links: [
-      { title: 'Kubernetes Essentials', href: '/docs/kubernetes-resources/kubernetes-essentials' },
-      { title: 'Troubleshooting', href: '/docs/kubernetes-resources/troubleshooting' },
+      {
+        title: 'Kubernetes Essentials',
+        href: '/docs/kubernetes-resources/kubernetes-essentials',
+      },
+      {
+        title: 'Troubleshooting',
+        href: '/docs/kubernetes-resources/troubleshooting',
+      },
     ],
   },
   {
     title: 'Reference',
     links: [
-      { title: 'Gimlet Manifest Reference', href: '/docs/reference/gimlet-manifest-reference' },
+      {
+        title: 'Gimlet Manifest Reference',
+        href: '/docs/reference/gimlet-manifest-reference',
+      },
       { title: 'CI plugins Reference', href: '/docs/reference/ci-plugins' },
-      { title: 'Gimlet Configuration Reference', href: '/docs/reference/gimlet-configuration-reference' },
-      { title: 'Onechart Reference', href: '/docs/reference/onechart-reference' },
+      {
+        title: 'Gimlet Configuration Reference',
+        href: '/docs/reference/gimlet-configuration-reference',
+      },
+      {
+        title: 'Onechart Reference',
+        href: '/docs/reference/onechart-reference',
+      },
     ],
   },
   {
     title: 'Learn More',
     links: [
-      { title: 'Gimlet Compared to', href: '/docs/learn-more/gimlet-compared-to' },
+      {
+        title: 'Gimlet Compared to',
+        href: '/docs/learn-more/gimlet-compared-to',
+      },
       { title: 'FAQ', href: '/docs/learn-more/faq' },
       { title: 'Contact us', href: '/docs/learn-more/contact-us' },
     ],
@@ -166,7 +263,7 @@ export default function App({ Component, pageProps }) {
   let isBackendPage = router.pathname === '/backend'
   let isAIPage = router.pathname === '/ai-deployment'
 
-  let image = "logosocial.png"
+  let image = 'logosocial.png'
   if (pageProps.markdoc?.frontmatter.image_social) {
     image = pageProps.markdoc?.frontmatter.image_social
   }
@@ -174,14 +271,17 @@ export default function App({ Component, pageProps }) {
     image = pageProps.markdoc?.frontmatter.image
   }
 
-  let tableOfContents = pageProps.markdoc?.frontmatter.toc !== false && pageProps.markdoc?.content
-    ? collectHeadings(pageProps.markdoc.content)
-    : []
+  let tableOfContents =
+    pageProps.markdoc?.frontmatter.toc !== false && pageProps.markdoc?.content
+      ? collectHeadings(pageProps.markdoc.content)
+      : []
 
-  const currentUrl = "https://gimlet.io" + router.pathname;
+  const currentUrl = 'https://gimlet.io' + router.pathname
 
-  let ogTitle = "Deploy and share your frontend, backend or AI project using open-source tooling and social auth."
-  let description = "Deploy and share your frontend, backend or AI project using open-source tooling and social auth."
+  let ogTitle =
+    'Deploy and share your frontend, backend or AI project using open-source tooling and social auth.'
+  let description =
+    'Deploy and share your frontend, backend or AI project using open-source tooling and social auth.'
   if (pageProps.markdoc?.frontmatter.description) {
     description = pageProps.markdoc?.frontmatter.description
   }
@@ -189,57 +289,59 @@ export default function App({ Component, pageProps }) {
   if (pageProps.markdoc?.frontmatter.title) {
     ogTitle = pageProps.markdoc?.frontmatter.title
     if (pageProps.markdoc?.frontmatter.title.length < 20) {
-      pageTitle = pageProps.markdoc?.frontmatter.title + " - " + pageTitle
+      pageTitle = pageProps.markdoc?.frontmatter.title + ' - ' + pageTitle
     } else {
-      pageTitle = pageProps.markdoc?.frontmatter.title + " - Gimlet"
+      pageTitle = pageProps.markdoc?.frontmatter.title + ' - Gimlet'
     }
   }
   if (pageProps.markdoc?.frontmatter.ogTitle) {
     ogTitle = pageProps.markdoc?.frontmatter.ogTitle
   }
   if (isYamlGeneratorPage) {
-    pageTitle = "Kubernetes YAML Generator"
-    ogTitle = "Kubernetes YAML Generator"
-    description = "Generate Kubernetes YAML files for web application deployments. Uses a generic Helm chart, because no one can remember the Kubernetes yaml syntax."
-    image = "yaml-generator.png"
+    pageTitle = 'Kubernetes YAML Generator'
+    ogTitle = 'Kubernetes YAML Generator'
+    description =
+      'Generate Kubernetes YAML files for web application deployments. Uses a generic Helm chart, because no one can remember the Kubernetes yaml syntax.'
+    image = 'yaml-generator.png'
   }
   if (isFrontendPage) {
-    pageTitle = "Frontend";
-    ogTitle = "Host Your Frontend Without Billing Surprises"
-    description = "Host Your Frontend Without Billing Surprises"
+    pageTitle = 'Frontend'
+    ogTitle = 'Host Your Frontend Without Billing Surprises'
+    description = 'Host Your Frontend Without Billing Surprises'
   }
   if (isBackendPage) {
-    pageTitle = "Backend";
-    ogTitle = "Deploy Containerized Backend Services"
-    description = "Deploy Containerized Backend Services"
+    pageTitle = 'Backend'
+    ogTitle = 'Deploy Containerized Backend Services'
+    description = 'Deploy Containerized Backend Services'
   }
   if (isAIPage) {
-    pageTitle = "AI";
-    ogTitle = "Introduce Kubernetes to Your AI Project"
-    description = "Introduce Kubernetes to Your AI Project"
+    pageTitle = 'AI'
+    ogTitle = 'Introduce Kubernetes to Your AI Project'
+    description = 'Introduce Kubernetes to Your AI Project'
   }
   if (isPricingPage) {
-    pageTitle = "Pricing";
-    ogTitle = "One Price Tag, One Year Access"
-    description = "For $300 a year, you’ll get every existing and brand new feature without usage restrictions."
+    pageTitle = 'Pricing'
+    ogTitle = 'One Price Tag, One Year Access'
+    description =
+      'For $300 a year, you’ll get every existing and brand new feature without usage restrictions.'
   }
 
   useEffect(() => {
     Fathom.load('TOOENNXR', {
-      excludedDomains: ['localhost', "127.0.0.1"],
-    });
+      excludedDomains: ['localhost', '127.0.0.1'],
+    })
 
     function onRouteChangeComplete() {
-      Fathom.trackPageview();
+      Fathom.trackPageview()
     }
     // Record a pageview when route changes
-    router.events.on('routeChangeComplete', onRouteChangeComplete);
+    router.events.on('routeChangeComplete', onRouteChangeComplete)
 
     // Unassign event listener
     return () => {
-      router.events.off('routeChangeComplete', onRouteChangeComplete);
-    };
-  }, []);
+      router.events.off('routeChangeComplete', onRouteChangeComplete)
+    }
+  }, [])
 
   return (
     <>
@@ -250,17 +352,25 @@ export default function App({ Component, pageProps }) {
         <meta name="description" content={description} />
         <meta content="Gimlet" property="og:site_name" />
 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
-        <meta content={pageTitle} property="og:title"/>
-        {isYamlGeneratorPage &&
+        <meta content={pageTitle} property="og:title" />
+        {isYamlGeneratorPage && (
           <meta
             name="keywords"
             content="kubernetes yaml, kubernetes deployment yaml, yaml generator, kubernetes yaml generator, k8s yaml generator, kubectl yaml, kubernetes yaml reference, k8s deployment yaml, yaml template generator, yaml editor for kubernetes, yaml kubernetes, kubernetes yaml templates, yaml editor, kubernetes manifest, web application deployment yaml, web application manifest"
           />
-        }
+        )}
         <meta content="website" property="og:type" />
-        <meta content={`https://api.placid.app/u/ghvjld730lsgd?title[text]=${encodeURI(ogTitle)}`} property="og:image" />
+        <meta
+          content={`https://api.placid.app/u/ghvjld730lsgd?title[text]=${encodeURI(
+            ogTitle
+          )}`}
+          property="og:image"
+        />
         <meta content={description} property="og:description" />
 
         <meta name="twitter:card" content="summary_large_image" />
@@ -268,10 +378,25 @@ export default function App({ Component, pageProps }) {
         <meta name="twitter:creator" content="@gimlet_io" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={`https://api.placid.app/u/ghvjld730lsgd?title[text]=${encodeURI(ogTitle)}`} />
+        <meta
+          name="twitter:image"
+          content={`https://api.placid.app/u/ghvjld730lsgd?title[text]=${encodeURI(
+            ogTitle
+          )}`}
+        />
 
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
 
         <link rel="canonical" href={currentUrl} />
